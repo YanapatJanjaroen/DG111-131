60625b6005776d5cc38849658b5bfea1b4334944
YanapatJanjaroen <skyyanapat2007@gmail.com>
2026-06-25 15:25:57 +0700

    Add initial VSCode configuration and sample C program for MSYS2 setup

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("สวัสดีครับ ผม ญานภัทร จันทร์เจริญ\n");
+    printf("รหัสนักศึกษา: 692110131\n");
+    printf("วิชา: DG111 Computer Programming\n");
+
+    return 0;
+}
