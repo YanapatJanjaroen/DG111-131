3ae93893d4010ceeaafc3cfbe1c8b381e5b69cee
YanapatJanjaroen <skyyanapat2007@gmail.com>
2026-07-16 16:26:08 +0700

    Refactor damage calculation in combat simulator for clarity and consistency

--- week04/ex03_damage_calc.c
+++ week04/ex03_damage_calc.c
@@ -3,6 +3,7 @@
 int main()
 {
     int player_attack, Enemy_defense, Hit_Number;
+    int enemy_hp = 500;
     printf("== COMBAT SIMULATOR ==\n");
     printf("Enter player's attack power: ");
     scanf("%d", &player_attack);
@@ -16,11 +17,15 @@ int main()
     {
         int damage = (int)ceil((float)base_damage * 1.5f);
         printf("Damage: %d *** Critical Hit! x1.5 ***\n", damage);
+        int current_hp = enemy_hp - damage;
+        printf("Enemy HP: %d\n", current_hp);
     }
     else
     {
         int damage = base_damage;
         printf("Damage: %d (Normal)\n", damage);
+        int current_hp = enemy_hp - damage;
+        printf("Enemy HP: %d\n", current_hp);
     }
 
     return 0;
